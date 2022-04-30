3184aba33b8bd7a221521c5758617ada4017383c
pankaj1707k <pankaj.1707k@gmail.com>
2022-04-30 22:04:03 +0530

    Upcoming Conferences page

--- incomasys/urls.py
+++ incomasys/urls.py
@@ -19,5 +19,6 @@ from main.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home, name='home')
+    path('', home, name='home'),
+    path('upcoming/', upcoming_conferences, name='upcoming-conf')
 ]
--- main/views.py
+++ main/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+
+def upcoming_conferences(request):
+    return render(request, 'upcoming_conferences.html')
